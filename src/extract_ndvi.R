d8acec536b4d0c40371093150b0a2ba3e8408a7d
Jiangyue-Wang <lily_wang@pku.edu.cn>
2023-05-29 12:57:45 +0800

    update code

---
+++ src/extract_ndvi.R
@@ -0,0 +1,12 @@
+### load library----------
+library(sf)
+library(terra)
+library(tidyverse)
+library(tidyterra)
+library(parallel)
+
+### parameter setting----------
+n_sample <- 10000
+
+### read in suli range--------
+
