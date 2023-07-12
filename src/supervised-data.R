eef5a2d20cb0dd5b43c899d2a6fd060f1d9edbbd
Jiangyue-Wang <lily_wang@pku.edu.cn>
2023-07-12 10:05:22 +0800

    add snow points, but not extract band values yet

--- src/supervised-data.R
+++ src/supervised-data.R
@@ -26,8 +26,25 @@ recordTable %>% mutate(DateTimeOriginal = lubridate::as_datetime(DateTimeOrigina
 
 Suli_points <- recordTable %>% mutate(DateTimeOriginal = lubridate::as_datetime(DateTimeOriginal)) %>%filter(Subject%in%c("Bareland", "Grassland", "Jiji Grass", "Seabuckthorn", "Shrubland"))%>%bind_rows(record_add) %>%mutate(Subject = as.factor(Subject))
 
-write.csv(Suli_points, "field-data/field-data.csv", row.names = F)
+# write.csv(Suli_points, "field-data/field-data.csv", row.names = F)
+Suli_points <- read.csv("field-data/field-data.csv")
+## read snow polygons
+snowdir <- "field-data/Snow/"
+snowfile <- dir(snowdir)
+for(i in 1:length(snowfile)){
+  tmp <- read_sf(paste0(snowdir, snowfile[i]))
+  tmp <- st_zm(tmp, drop = T, what = "ZM")
+  assign(paste0("Snow",i), tmp)
+}
+snow <- st_union(bind_rows(Snow1,Snow2,Snow3,Snow4,Snow5))
+points_num <- round(nrow(Suli_points)/length(unique(Suli_points$Subject)),-1)
+set.seed(321)
+snow_points <- st_sample(snow, size = points_num) %>% unlist() %>% matrix(ncol=2, byrow = T)
 
+snow_rows <- data.frame(SourceFile = "GoogleEarth", DateTimeOriginal = NA, Subject = "Snow", GPSLatitude = snow_points[,2], GPSLongitude = snow_points[,1])
+
+Suli_points <- bind_rows(Suli_points, snow_rows)
+## write shapefile
 st_write(st_as_sf(Suli_points[,3:5], coords = c("GPSLongitude", "GPSLatitude"))%>%st_set_crs(4326), "field-data/field-data.shp", append=F)
 # ext <- sf::st_bbox(st_as_sf(Suli_points, coords = c("GPSLongitude", "GPSLatitude"))) %>% sf::st_set_crs(4326)
 ext <- st_bbox(st_read("downloaded-datasets/Suli/suli.shp"))
